8c431c8b12e88bdb11ad9a2f78ace1edafb18eee
JVercout <jerome.vercoutere@gmail.com>
2015-06-16 09:34:18 +0200

    refactor

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('socks5.js');
+  api.addFiles('lib/server/socks5.js');
   api.export(['Agent'], 'server');
 });
 
